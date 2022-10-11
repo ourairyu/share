ae6bb575de9e09290a251f455112ac9eafe778f2
Ourai Lin <ourairyu@gmail.com>
2022-10-12 04:29:34 +0800

    Generate on Wed Oct 12 2022 04:29:34 GMT+0800 (China Standard Time)

---
+++ assets/pages/doc-c880dc4660dee6b589b91fa90d96d4362f806002998d7fba77f76c8db76c973d.js
@@ -0,0 +1,1 @@
+(function(){var t,e,o;t=$("#__ourai_doc__"),o=t.attr("data-code"),o&&decodeURIComponent(location.search.slice(1).split("&")[0]).split("|").filter(function(t){return!!t}).map(function(t){return String.fromCharCode(Number(t))}).join("")===o&&(e=$("#sharedDoc"),e.html(t.html()),$("table",e).addClass("table table-bordered")),t.remove()}).call(this);
